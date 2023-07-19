5cf0f28121fdc65061f7a1cb45e0fe9d5ba41d5a
ansa-aboudou <54751685+ansa-aboudou@users.noreply.github.com>
2023-07-19 22:30:50 +0900

    Update page_explore.py

--- page_explore.py
+++ page_explore.py
@@ -271,49 +271,45 @@ def page_explore():
             stat_dist = dist.rvs(*c_params[j][0:(len(*c_params)-2)], loc = c_params[0][-2],
              scale = c_params[0][-1], size=20000)
 
-            def custo_distrib(r):
-                if type(p_over) == type(st.empty()):
-                    p_over = 0
-                if type(p_0) == type(st.empty()):
-                    p_0 = 0
-                
+            if type(p_over) == type(st.empty()):
+                p_over = 0
+            if type(p_0) == type(st.empty()):
+                p_0 = 0
+            
+            if p_0 != 0:
+                condi_0 = (r <= 0)
+            else:
+                condi_0 = (r < 0)
+            # Loop to replace negative values and values > 200
+            while any(condi_0 | (r >= 200)):
+                invalid_indices = condi_0 | (r >= 200)
+                size_invalid = invalid_indices.sum()
+                r_invalid = dist.rvs(*c_params[j][0:(len(*c_params)-2)], loc = c_params[0][-2],
+                         scale = c_params[0][-1], size=size_invalid)
+                r[invalid_indices] = r_invalid
                 if p_0 != 0:
                     condi_0 = (r <= 0)
                 else:
                     condi_0 = (r < 0)
-                # Loop to replace negative values and values > 200
-                while any(condi_0 | (r >= 200)):
-                    invalid_indices = condi_0 | (r >= 200)
-                    size_invalid = invalid_indices.sum()
-                    r_invalid = dist.rvs(*c_params[j][0:(len(*c_params)-2)], loc = c_params[0][-2],
-                             scale = c_params[0][-1], size=size_invalid)
-                    r[invalid_indices] = r_invalid
-                    if p_0 != 0:
-                        condi_0 = (r <= 0)
-                    else:
-                        condi_0 = (r < 0)
+          
+            if p_over != 0:
+                dist_instance_over = getattr(stats, "uniform")
+                dict_distr_over = {"loc": p_over_loc, "scale": p_over_scale}
+                rv_over = dist_instance_over(**dict_distr_over)
+                dict_distr_over["size"] = size
+                r_over = dist_instance_over.rvs(**dict_distr_over)
+
+            # Create an array of values [0, 1, 2] with corresponding probabilities
+            values = [0, 1, -999]
+            probabilities = [p_0 / 100.0, 1 - (p_0 / 100.0 + p_over / 100.0), p_over / 100.0]
+            # Generate random samples from the Bernoulli distribution
+            samples = np.random.choice(values, size=size, p=probabilities).astype(float)
+            # Replace values equal to 1 with normal distribution values
+            samples[samples == 1] = r[samples == 1]
+            # Replace values equal to 2 with uniform distribution values
+            if p_over != 0:
+                samples[samples == -999] = r_over[samples == -999]
               
-                if p_over != 0:
-                    dist_instance_over = getattr(stats, "uniform")
-                    dict_distr_over = {"loc": p_over_loc, "scale": p_over_scale}
-                    rv_over = dist_instance_over(**dict_distr_over)
-                    dict_distr_over["size"] = size
-                    r_over = dist_instance_over.rvs(**dict_distr_over)
-    
-                # Create an array of values [0, 1, 2] with corresponding probabilities
-                values = [0, 1, -999]
-                probabilities = [p_0 / 100.0, 1 - (p_0 / 100.0 + p_over / 100.0), p_over / 100.0]
-                # Generate random samples from the Bernoulli distribution
-                samples = np.random.choice(values, size=size, p=probabilities).astype(float)
-                # Replace values equal to 1 with normal distribution values
-                samples[samples == 1] = r[samples == 1]
-                # Replace values equal to 2 with uniform distribution values
-                if p_over != 0:
-                    samples[samples == -999] = r_over[samples == -999]
-                return samples
-              
-            samples = custo_distrib(r)
-            stat_dist = custo_distrib(stat_dist)
             mean = round( np.mean( stat_dist ), 2)
             std = round( np.std( stat_dist ), 2)
 
