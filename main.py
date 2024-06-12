7f225bd93f4ab00afe141c43367401a8d3581597
ansa-aboudou <54751685+ansa-aboudou@users.noreply.github.com>
2024-06-12 12:58:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def main():
             If you like this app please star its
             [**GitHub**](https://github.com/rdzudzar/DistributionAnalyser)
             repo, share it and feel free to open an issue if you find a bug 
-            or if you want some additional features.
+            or if you want some additional features. ++
             """
     )
 
